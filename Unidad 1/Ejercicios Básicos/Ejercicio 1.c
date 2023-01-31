68c1dba29fbf47a4b814ed719f2d1bd09eeb86df
arturocadenamendez06 <112593388+arturocadenamendez06@users.noreply.github.com>
2023-01-30 20:03:32 -0600

    Update Ejercicio 1.c

--- Unidad 1/Ejercicios Básicos/Ejercicio 1.c
+++ Unidad 1/Ejercicios Básicos/Ejercicio 1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(int argc, char *argv[]) {
+	int numero1, numero2;
+	puts("Escribir el primer número: ");
+	scanf("%d",&numero1);
+	puts("Escribir el segundo número: ");
+	scanf("%d",&numero2);
+	if(numero1 == numero2){
+		printf("Los números son iguales");
+	}
+	else{
+		if(numero1 > numero2){
+			printf("El número %d es mayor que el número %d",numero1,numero2);
+		}
+		else{
+			printf("El número %d es mayor que el número %d",numero2,numero1);
+		}
+	}
+	return 0;
+}
