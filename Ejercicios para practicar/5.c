c51fbbb54d44130ec53abe4e1dd8142412c4a920
arturocadenamendez06 <112593388+arturocadenamendez06@users.noreply.github.com>
2023-01-30 23:10:11 -0600

    Create 5.c

---
+++ Ejercicios para practicar/5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/*Escribir un programa que realice la operación módulo mediante operaciones sucesivas de resta. En
+	programación el módulo de un número es el resto de la división de dicho número entre otro número.*/
+	int numero,divisor;
+	printf("Escribir el número: ");
+	scanf("%d",&numero);
+	printf("Escribir el divisor: ");
+	scanf("%d",&divisor);
+	while(numero >= divisor){
+		numero = numero - divisor;
+	}
+	printf("El módulo es: %d",numero);
+	return 0;
+}
