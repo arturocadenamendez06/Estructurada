3bf1ef8d21bdc982f91620bcf42dd8bf3d2eb83e
arturocadenamendez06 <112593388+arturocadenamendez06@users.noreply.github.com>
2023-02-01 23:43:02 -0600

    Update 15.c

--- Ejercicios para practicar/15.c
+++ Ejercicios para practicar/15.c
@@ -1,48 +1,26 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	/*Escriba un programa que reciba tres calificaciones de un estudiante y devuelva el promedio, la*/
-	/*máxima y la mínima calificación.*/
-  
-	int accion, tirog=0,tirod=0,fr=0,fc=0,ga=0,gr=0,a=0,e=0;
+	/*El equipo de futbol soccer VENADOS requiere llevar un conteo de las acciones que realiza durante un
+	juego, las acciones están catalogadas como sigue: 1)Tiro a gol, 2)Tiro desviado, 3)Falta recibida,
+	4)Falta cometida, 5)Gol anotado, 6)Gol recibido, 7)Amonestado, 8)Expulsado, 0)Fin del juego.
+	Elaborar un programa que permita capturar las acciones que se den en el partido, por cada acción se
+	debe introducir el número de acción correspondiente, al escribir la acción 0, se deben imprimir las
+	estadísticas del juego.*/
 	
-	do{
-		printf("Escribir la acción: ");
-		scanf("%d",&accion);
-		switch(accion){
-		case 1:
-			tirog = tirog + 1;
-			break;
-		case 2:
-			tirod = tirod + 1;
-			break;
-		case 3:
-			fr = fr + 1;
-			break;
-		case 4:
-			fc = fc + 1;
-			break;
-		case 5:
-			ga = ga + 1;
-			break;
-		case 6:
-			gr = gr + 1;
-			break;
-		case 7:
-			a = a + 1;
-			break;
-		case 8:
-			e = e + 1;
-			break;
-		}
-	} while(accion != 0);
-	printf("Tiro a gol: %d\n",tirog);
-	printf("Tiro desviado: %d\n",tirod);
-	printf("Falta recibida: %d\n",fr);
-	printf("Falta cometida: %d\n",fc);
-	printf("Gol anotado: %d\n",ga);
-	printf("Gol recibido: %d\n",gr);
-	printf("Amonestado: %d\n",a);
-	printf("Expulsado: %d\n",e);
+	float r1,r2,a1,a2,a;
+	printf("Escribir el primer radio (circunferencia interna): ");
+	scanf("%f",&r1);
+	printf("Escribir el segundo radio (circunferencia externa): ");
+	scanf("%f",&r2);
+	if(r1 >= r2){
+		printf("El radio interno no puede ser mayor que el externo");
+	}
+	else{
+		a1 = 3.1416 * r1 * r1;
+		a2 = 3.1416 * r2 * r2;
+		a = a2 - a1;
+		printf("Area sombreada: %.4f",a);
+	}
 	return 0;
 }
